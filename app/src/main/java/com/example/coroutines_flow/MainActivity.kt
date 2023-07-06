fd8db3a9b2c76e4b12585827448185529043c767
Denis Izmaylovich <drakoshago6@gmail.com>
2023-07-06 22:05:39 +0300

    Bug fix

--- app/src/main/java/com/example/coroutines_flow/MainActivity.kt
+++ app/src/main/java/com/example/coroutines_flow/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.animateOffsetAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -17,6 +18,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
@@ -27,8 +29,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -39,12 +39,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.coroutines_flow.ui.theme.Coroutines_FlowTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 import kotlin.random.Random
@@ -52,23 +52,20 @@ import kotlin.random.Random
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val data = flow {
-                val dataFlow = (0 .. 40).map {
-                    Item(
-                        height = Random.nextInt(540).dp,
-                        color = Color(Random.nextInt())
-                    )
-                }
-                emit(dataFlow)
-
+        val list = (0..40).map {
+            Item(
+                height = Random.nextInt(540).dp,
+                color = Color(Random.nextInt())
+            )
         }
+
+
 
 
 
         setContent {
             Coroutines_FlowTheme {
 
-                val list = data.collectAsState(initial = listOf())
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -77,11 +74,11 @@ class MainActivity : ComponentActivity() {
 
                     val lazyColumnState = rememberLazyListState()
                     SliderLazyColumn(
-                        key = list.value,
+                        key = list,
                         state = lazyColumnState,
                         context = LocalContext.current
                     ) {
-                        items(list.value) { item ->
+                        items(list) { item ->
                             Box(
                                 modifier = Modifier
                                     .fillMaxSize()
@@ -91,7 +88,11 @@ class MainActivity : ComponentActivity() {
                                 Text(text = "BOX ${item.height}")
                             }
                         }
-                        items(list.value, key = { it.height.value * it.color.toArgb() }) { item ->
+                        items(10){
+                            Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription ="",
+                                modifier = Modifier.padding(16.dp))
+                        }
+                        items(list, key = { it.height.value * it.color.toArgb() }) { item ->
                             Box(
                                 modifier = Modifier
                                     .fillMaxSize()
@@ -101,6 +102,11 @@ class MainActivity : ComponentActivity() {
                                 Text(text = "BOX ${item.height}")
                             }
                         }
+                        items(10){
+                            Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription ="",
+                            modifier = Modifier.padding(16.dp))
+                        }
+
 
                     }
                 }
@@ -112,7 +118,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SliderLazyColumn(
-    key: List<Item>,
+    key: Any?,
     state: LazyListState,
     context: Context,
     content: LazyListScope.() -> Unit
@@ -120,17 +126,15 @@ fun SliderLazyColumn(
     suspend fun getStages(lazyListState: LazyListState, density: Float): List<Dp> {
         val stages = mutableListOf<Dp>()
         stages.add(0.dp)
-        stages.add(0.dp)
         var height: Dp = 0.dp
-
-        lazyListState.scrollToItem(lazyListState.layoutInfo.totalItemsCount )
+        lazyListState.scrollToItem(lazyListState.layoutInfo.totalItemsCount)
         val lastOrPreLastItemIndex = lazyListState.firstVisibleItemIndex
         val offsetLast = (lazyListState.firstVisibleItemScrollOffset / density).dp
 
-        for (item in 1 until lastOrPreLastItemIndex) {
+        for (item in 0 until lastOrPreLastItemIndex) {
             lazyListState.scrollToItem(item)
 
-            var currentHeight =
+            val currentHeight =
                 (lazyListState.layoutInfo.visibleItemsInfo.first().size / density).roundToInt().dp
             stages.add(height + currentHeight)
             height += currentHeight
@@ -138,6 +142,7 @@ fun SliderLazyColumn(
         stages.add(height + offsetLast)
         return stages
     }
+
     val density = remember { context.resources.displayMetrics.density }
 
     val pointSize = remember {
@@ -150,10 +155,6 @@ fun SliderLazyColumn(
         mutableStateOf(mutableListOf(0.dp))
     }
     val heightScreen = LocalConfiguration.current.screenHeightDp
-
-    val key1 = remember {
-        mutableStateOf(true)
-    }
 
 
     val rowAlpha = animateFloatAsState(
@@ -200,7 +201,8 @@ fun SliderLazyColumn(
         if (stagesOfDp.value.size > 0) {
             sliderOffset.value = animateOffsetAsState(
                 targetValue = if (!firstVisibleItemIndex.value.dp.value.isNaN() && !stagesOfDp.value[firstVisibleItemIndex.value].value.isNaN()
-                    && !multiplier.value.isInfinite() ) {
+                    && !multiplier.value.isInfinite()
+                ) {
                     Offset(
                         x = 0f,
                         y = (firstVisibleItemScrollOffset.value.dp.value / density + stagesOfDp.value[firstVisibleItemIndex.value].value) * multiplier.value
@@ -216,16 +218,8 @@ fun SliderLazyColumn(
                 .fillMaxWidth(0.97f)
                 .fillMaxHeight(), state = state
         ) {
-            item {
-                SideEffect {
-                    key1.value = !key1.value
-                }
-            }
-
-
             content.invoke(this)
         }
-
 
         val worstOffset = remember {
             derivedStateOf {
